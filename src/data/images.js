67a60314f6bd0a90000b6c44f8754f6c5bbaed2b
Martyna Machowiak <martyna.machowiak@gear4music.com>
2020-08-18 00:21:55 +0100

    Add mock data

---
+++ src/data/images.js
@@ -0,0 +1,19 @@
+const images = [
+  {
+    url: "https://images-assets.nasa.gov/image/PIA12235/PIA12235~thumb.jpg",
+    description: "Nearside of the Moon",
+  },
+  {
+    url: "https://images-assets.nasa.gov/image/PIA13517/PIA13517~thumb.jpg",
+    description: "Color of the Moon",
+  },
+  {
+    url: "https://images-assets.nasa.gov/image/PIA12229/PIA12229~thumb.jpg",
+    description: "Mineral Mapping the Moon",
+  },
+  {
+    url: "https://images-assets.nasa.gov/image/PIA12233/PIA12233~thumb.jpg",
+    description: "Mapping the Moon, Point by Point",
+  },
+];
+export default images;
