182fd1aed227d9142088c304c44b48f00fee535b
Martyna Machowiak <martyna.machowiak@gear4music.com>
2020-08-17 21:04:51 +0100

    Rename <App/> to <AppEvents/> for different App variations

---
+++ src/App/AppEvents.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>App - Events</h1>
+
+      <div></div>
+    </div>
+  );
+};
+
+export default App;
