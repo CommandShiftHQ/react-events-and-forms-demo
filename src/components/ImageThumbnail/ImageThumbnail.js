3bcbef21b82a90ce76eedf6893b895720a4a0093
Martyna Machowiak <martyna.machowiak@gear4music.com>
2020-08-18 01:03:18 +0100

    Add event handlers to <ImageThumbnail />

--- src/components/ImageThumbnail/ImageThumbnail.js
+++ src/components/ImageThumbnail/ImageThumbnail.js
@@ -5,7 +5,12 @@ const ImageThumbnail = ({ url, description }) => {
   const [save, setSave] = useState(false);
 
   return (
-    <div className="image-thumbnail">
+    <div
+      className="image-thumbnail"
+      onMouseOver={() => setHover(true)}
+      onMouseOut={() => setHover(false)}
+      onClick={() => setSave(!save)}
+    >
       <img src={url} className="image-thumbnail__image" alt={description} />
       {save && <div className="image-thumbnail__save">Saved!</div>}
       {hover && <div className="image-thumbnail__title">{description}</div>}
