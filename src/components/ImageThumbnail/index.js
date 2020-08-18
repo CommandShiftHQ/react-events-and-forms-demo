9d3c05ad424de9dca533c25c21a0518f0cc0f765
Martyna Machowiak <martyna.machowiak@gear4music.com>
2020-08-18 21:11:23 +0100

    Add event target investigation to button onClick handler

--- src/components/ImageThumbnail/index.js
+++ src/components/ImageThumbnail/index.js
@@ -5,12 +5,19 @@ const ImageThumbnail = ({ url, description }) => {
   const [hover, setHover] = useState(false);
   const [save, setSave] = useState(false);
 
+  const handleClick = (event) => {
+    const target = event.target;
+    const alt = target.getAttribute("alt");
+    console.log(target, alt);
+    setSave(!save);
+  };
+
   return (
     <div
       className="image-thumbnail"
       onMouseOver={() => setHover(true)}
       onMouseOut={() => setHover(false)}
-      onClick={() => setSave(!save)}
+      onClick={(event) => handleClick(event)}
     >
       <img src={url} className="image-thumbnail__image" alt={description} />
       {save && <div className="image-thumbnail__save">Saved!</div>}
