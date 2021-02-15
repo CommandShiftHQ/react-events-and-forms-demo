cb0af73740ccf2fc368c7ba1bd7dcea6903e3e35
Martyna Machowiak <martyna.machowiak@gear4music.com>
2021-02-15 16:07:11 +0000

    Clean for events demo

--- src/components/EventsDemo/ImageThumbnail/index.js
+++ src/components/EventsDemo/ImageThumbnail/index.js
@@ -1,27 +1,13 @@
-import React, { useState } from "react";
+import React from "react";
 import "./style.css";
 
 const ImageThumbnail = ({ url, description }) => {
-  const [hover, setHover] = useState(false);
-  const [selected, setSelected] = useState(false);
-
-  const handleClick = (event) => {
-    const target = event.target;
-    const alt = target.getAttribute("alt");
-    console.log(target, alt);
-    setSelected(!selected);
-  };
 
   return (
-    <div
-      className="image-thumbnail"
-      onMouseOver={() => setHover(true)}
-      onMouseOut={() => setHover(false)}
-      onClick={(event) => handleClick(event)}
-    >
+    <div className="image-thumbnail">
       <img src={url} className="image-thumbnail__image" alt={description} />
-      {selected && <div className="image-thumbnail__save">Selected</div>}
-      {hover && <div className="image-thumbnail__title">{description}</div>}
+      <div className="image-thumbnail__save">Selected</div>
+      <div className="image-thumbnail__title">{description}</div>
     </div>
   );
 };
