7b2d2e4fe109e2713575e42c3e26f200aa2e8603
Martyna Machowiak <martyna.machowiak@gear4music.com>
2021-02-15 16:13:22 +0000

    Set to show only Events demo

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 import EventsDemo from "./components/EventsDemo";
-import FormDemo from "./components/FormDemo";
 import "./App.css";
 
 const App = () => {
-  const [demo, setDemo] = useState("Events");
-
-  const handleClick = () => {
-    demo === "Events" ? setDemo("Form") : setDemo("Events");
-  };
-
   return (
     <div className="App">
-      <button onClick={() => handleClick()}>Change to {demo} demo</button>
-      {demo === "Events" ? <EventsDemo /> : <FormDemo />}
+      <EventsDemo />
     </div>
   );
 };
